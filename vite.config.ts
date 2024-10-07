c89a4aeeefbe3e255a77eba63175e2d964d12f66
hunterkilltree <snguoila@yahoo.com>
2024-10-07 23:09:54 +0700

    Correct config dts to ship type definitions

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,19 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"] })],
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: "./tsconfig.lib.json",
+    }),
+  ],
   build: {
     copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, "src/lib/main.ts"),
       formats: ["es"],
+      fileName: "ui-lib",
+      name: "UILib",
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime"],
